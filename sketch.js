3a80fefbda0810a8d06dbabbcba54f7fb9f7f4bd
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2022-04-11 20:03:54 +0200

    draw debug dots

--- sketch.js
+++ sketch.js
@@ -1,10 +1,7 @@
-
-
-// https://editor.p5js.org/pixelfelt/sketches/oS5CwSbM1
+const DEBUG = true;
 const WIDTH = 640;
 const HEIGHT = 480;
-let MAGIC_CIRCLE_SIZE = WIDTH / 4;
-
+const DBG_CIRCLE_SIZE = 8;
 function setup() {
 
   hf = new Handsfree({ hands: true })
@@ -19,21 +16,18 @@ function setup() {
         handsPositions = [];
         break;
       case 1:
-        handsPositions = [data.hands.multiHandLandmarks[0][21]]
-        let l = data.hands.multiHandLandmarks[0][4]
-        let r = data.hands.multiHandLandmarks[0][20]
-        MAGIC_CIRCLE_SIZE = WIDTH * abs(l.x - r.x);
+        handsPositions = [handStateFromLandmarks(data.hands.multiHandLandmarks[0])]
         break;
       case 2:
-        handsPositions = [data.hands.multiHandLandmarks[0][21], data.hands.multiHandLandmarks[1][21]]
+        handsPositions = [handStateFromLandmarks(data.hands.multiHandLandmarks[0]),
+        handStateFromLandmarks(data.hands.multiHandLandmarks[1])]
         break;
     }
-
-    // get gesture category(calulate based on arcs between fingers and state (curled or straightened))
-    // append to structure {gesture + how many times in the row} (append only if first sequence is from starter)
-    // verify if gestures in array are from sequence 
-
   })
+
+  // get gesture category(calulate based on arcs between fingers and state (curled or straightened))
+  // append to structure {gesture + how many times in the row} (append only if first sequence is from starter)
+  // verify if gestures in array are from sequence 
 
   createCanvas(WIDTH, HEIGHT);
   background(0, 0, 0, 0.1);
@@ -49,13 +43,43 @@ function setup() {
 function draw() {
   // setup camera
   image(video, 0, 0);
-  for (hand of handsPositions) {
-    image(mgc, WIDTH * hand.x - MAGIC_CIRCLE_SIZE / 2, HEIGHT * hand.y - MAGIC_CIRCLE_SIZE / 2,
-      MAGIC_CIRCLE_SIZE, MAGIC_CIRCLE_SIZE);
-  }
+  drawDebugDots();
+  drawMagicCircles();
+
+
+
   // tint(255, 0, 255);
 
   // if (hf.data?.hands?.landmarksVisible[0]) {
   //   console.log(hf.data.hands.landmarks[0])
   // }
+}
+function drawDebugDots() {
+  if (hf?.data?.hands?.multiHandLandmarks !== undefined && DEBUG) {
+    fill(244, 17, 17)
+    for (handLandmarks of hf?.data?.hands?.multiHandLandmarks) {
+      for (handLandmark of handLandmarks) {
+        circle(handLandmark.x * WIDTH,
+          handLandmark.y * HEIGHT,
+          DBG_CIRCLE_SIZE);
+      }
+    }
+  }
+}
+
+function drawMagicCircles() {
+  for (hand of handsPositions) {
+    let size = hand.size * WIDTH;
+    image(mgc, WIDTH * hand.x - size / 2, HEIGHT * hand.y - size / 2,
+      size, size);
+  }
+}
+
+function handStateFromLandmarks(s) {
+  return {
+    x: s[21].x,
+    y: s[21].y,
+    size: sqrt((s[4].x - s[20].x) * (s[4].x - s[20].x) +
+      (s[4].y - s[20].y) * (s[4].y - s[20].y))
+  };
 }
