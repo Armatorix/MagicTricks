8b6d81cbaefa9cace3e84bfe2cd447ab45c73db2
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2022-08-14 12:47:08 +0200

    refactor hands collectors

--- sketch.js
+++ sketch.js
@@ -2,8 +2,55 @@ const DEBUG = true;
 const WIDTH = 640;
 const HEIGHT = 480;
 const DBG_CIRCLE_SIZE = 8;
+const LEFT_IDX = 0;
+const RIGHT_IDX = 1;
 
-var rotation = 0;
+class MyHand {
+  constructor(side) {
+    this.side = side;
+    this.magicEnabled = false;
+    this.gestures = [];
+    this.drawable = false;
+    this.rotation = 0;
+    this.confidence = 0;
+
+
+    this.size = 0;
+    this.x = 0;
+    this.y = 0;
+  }
+
+  // catchGesture();
+  updateWithLandmarks(landmarks, confidence) {
+    this.rotation += random(-0.02, 0.12);
+    this.confidence = confidence;
+    if (landmarks === null) {
+      this.drawable = false;
+      return;
+    }
+    this.x = landmarks[21].x;
+    this.y = landmarks[21].y;
+    this.size = maxDistance(landmarks);
+    this.drawable = true;
+  }
+
+  draw() {
+    if (this.drawable) {
+      let size = this.size * 2.2;
+      push();
+      imageMode(CENTER);
+      translate(WIDTH * this.x, HEIGHT * this.y);
+      rotate(this.rotation);
+      image(mgc, 0, 0, size, size);
+      pop();
+
+    }
+  }
+}
+
+var hands = [new MyHand("left"), new MyHand("right")];
+
+
 
 function setup() {
 
@@ -23,28 +70,89 @@ function setup() {
       video: video,
     },
   })
-  hf.enablePlugins('browser')
   hf.start()
   handsPositions = []
-
-  hf.use('gestureColletor', data => {
-    switch (data?.hands?.multiHandLandmarks?.length) {
-      case undefined:
-        return;
-      case 0:
-        handsPositions = [];
-        break;
-      case 1:
-        handsPositions = [handStateFromLandmarks(data.hands.multiHandLandmarks[0])]
-        break;
-      case 2:
-        handsPositions = [handStateFromLandmarks(data.hands.multiHandLandmarks[0]),
-        handStateFromLandmarks(data.hands.multiHandLandmarks[1])
-        ]
-        break;
-    }
+  hf.useGesture({
+    "name": "fist",
+    "algorithm": "fingerpose",
+    "models": "hands",
+    "confidence": 7.5,
+    "description": [
+      [
+        "addCurl",
+        "Index",
+        "FullCurl",
+        1
+      ],
+      [
+        "addCurl",
+        "Middle",
+        "FullCurl",
+        1
+      ],
+      [
+        "addCurl",
+        "Ring",
+        "FullCurl",
+        1
+      ],
+      [
+        "addCurl",
+        "Pinky",
+        "FullCurl",
+        1
+      ],
+      [
+        "setWeight",
+        "Index",
+        2
+      ]
+    ]
   })
 
+
+  hf.use('positionCollector', data => {
+    let lLandmarks = null;
+    let lConfidance = 0;
+    let rLandmarks = null;
+    let rConfidance = 0;
+    switch (data?.hands?.multiHandLandmarks?.length) {
+      case undefined:
+        break;
+      case 0:
+        break;
+      case 1:
+        if (data.hands.multiHandedness[0].label === "Right") {
+          rLandmarks = data.hands.multiHandLandmarks[0]
+          rConfidance = data.hands.multiHandedness[0].confidence
+        } else {
+          lLandmarks = data.hands.multiHandLandmarks[0]
+          lConfidance = data.hands.multiHandedness[0].confidence
+        }
+        break;
+      case 2:
+        if (data.hands.multiHandedness[0].label === "Right") {
+          rLandmarks = data.hands.multiHandLandmarks[0]
+          rConfidance = data.hands.multiHandedness[0].confidence
+
+          lLandmarks = data.hands.multiHandLandmarks[1]
+          lConfidance = data.hands.multiHandedness[1].confidence
+        } else {
+          lLandmarks = data.hands.multiHandLandmarks[0]
+          lConfidance = data.hands.multiHandedness[0].confidence
+
+          rLandmarks = data.hands.multiHandLandmarks[1]
+          rConfidance = data.hands.multiHandedness[1].confidence
+        }
+        break;
+    }
+    hands[LEFT_IDX].updateWithLandmarks(lLandmarks, lConfidance);
+    hands[RIGHT_IDX].updateWithLandmarks(rLandmarks, rConfidance);
+  })
+
+  hf.use('gestureCollector', data => {
+    console.log(data.hands)
+  })
   // get gesture category(calulate based on arcs between fingers and state (curled or straightened))
   // append to structure {gesture + how many times in the row} (append only if first sequence is from starter)
   // verify if gestures in array are from sequence 
@@ -53,52 +161,15 @@ function setup() {
 
 function draw() {
   // setup camera
-  rotation += 0.1 * random(0, 1);
   image(video, 0, 0);
-  drawDebugDots();
-  drawMagicCircles();
 
-
-
-  // tint(255, 0, 255);
-
-  // if (hf.data?.hands?.landmarksVisible[0]) {
-  //   console.log(hf.data.hands.landmarks[0])
-  // }
-}
-
-function drawDebugDots() {
-  if (hf?.data?.hands?.multiHandLandmarks !== undefined && DEBUG) {
-    fill(244, 17, 17)
-    for (handLandmarks of hf.data.hands.multiHandLandmarks) {
-      for (handLandmark of handLandmarks) {
-        circle(handLandmark.x * WIDTH,
-          handLandmark.y * HEIGHT,
-          DBG_CIRCLE_SIZE);
-      }
-    }
+  for (hand of hands) {
+    hand.draw();
   }
 }
 
-function drawMagicCircles() {
-  for (hand of handsPositions) {
-    size = hand.size * 2.2;
-    push();
-    imageMode(CENTER);
-    translate(WIDTH * hand.x, HEIGHT * hand.y);
-    rotate(rotation);
-    image(mgc, 0, 0, size, size);
-    pop();
-  }
-}
 
-function handStateFromLandmarks(s) {
-  return {
-    x: s[21].x,
-    y: s[21].y,
-    size: maxDistance(s),
-  }
-}
+
 
 function maxDistance(s) {
   m = 0
